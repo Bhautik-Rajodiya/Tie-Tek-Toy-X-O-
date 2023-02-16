370bb4691ab17bce091d143a9f85384f097a50c6
Bhautik Rajodiya <prof.rw1bhautik@gmail.com>
2023-02-16 11:39:06 +0530

    Initial Commit

---
+++ Game 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Game 2
+//
+//  Created by Bhautik Rajodiya on 16/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
